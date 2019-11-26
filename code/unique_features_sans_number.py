d00f1e4472cde8f8f4629def38b81061401f6cfd
Zunair <zunair_ahmed@bitbucket.org>
2019-11-26 15:14:48 +0500

    comments added

--- code/unique_features_sans_number.py
+++ code/unique_features_sans_number.py
@@ -1,20 +1,27 @@
+# import relavent libraries
 import csv
 
+# Set path variables and list variab;e
 path_feature_file_csv = "./resources/cuckoo_attributes.csv"
 
 temp_csv = []
 
+# open file to read CSV
 with open(path_feature_file_csv) as feature_file_csv:
     
     features = csv.reader( feature_file_csv, delimiter=',' )
 
+    # loop over all the nested features
     for index_r, row in enumerate(features):
         print( "row: {} and Value: {}" .format( index_r, row[0] ) )
 
         temp_row = ""
         
+        # split the feature from '.'
         split_feature = row[0].split('.')
 
+        # loop over the separated feature and append 
+        # to temporary variable, if it is not a digit
         for index_w, word in enumerate( split_feature ) :
             
             if not word.isdigit():
@@ -28,8 +35,10 @@ with open(path_feature_file_csv) as feature_file_csv:
 
         temp_csv.append( temp_row )
 
+    # getiing unique values by type casting into Python set
     unique_features = list( set( temp_csv ) )
 
+    # open file and write the unique features in csv
     with open("./resources/unique_features.csv", 'w') as unique_feature_file_csv:
 
         wr = csv.writer(unique_feature_file_csv, delimiter='\n')
